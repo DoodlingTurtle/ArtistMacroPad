8140f38bca88e842ea495a1a64471b8a78ca78b1
DoodlingTurtle <52770682+DoodlingTurtle@users.noreply.github.com>
2021-11-13 05:51:38 +0100

    changes to window position and size are now stored. Each profile will have its own config

--- src/main.cpp
+++ src/main.cpp
@@ -14,16 +14,23 @@ int main( int argc, char** argv )
     std::string nextWindow = "./default.ini";
     if ( argc == 2 ) { nextWindow = argv[ 1 ]; }
 
-    int ret = 0;
+    int ret = 0, winWidth, winHeight, viewWidth, viewHeight, winX, winY;
 
     while ( nextWindow.length() > 0 ) {
+        Engine game;
+
+        // Reset window config values
+        winWidth  = 0;
+        winHeight = 0;
+        winX      = 0;
+        winY      = SDL_WINDOWPOS_CENTERED_DISPLAY( 0 );
 
         std::vector<std::string> iniPath;
         Utils::stringSplit( nextWindow, "/", iniPath, false );
 
+        // Read new Profile
         Utils::IniType* ini = new Utils::IniType();
         if ( !Utils::readIni( nextWindow, *ini ) ) return -1;
-        nextWindow = "";
 
         Utils::IniGroup win = Utils::readIniGroup( *ini, "window" );
         if ( win.size() == 0 ) {
@@ -31,26 +38,62 @@ int main( int argc, char** argv )
             delete ini;
             return -1;
         }
+
+        viewWidth  = Utils::readIniGroupInt( win, "width", 100 );
+        viewHeight = Utils::readIniGroupInt( win, "height", 100 );
+
         ini->erase( ini->find( "window" ) );
 
-        int winWidth  = Utils::readIniGroupInt( win, "width", 100 );
-        int winHeight = Utils::readIniGroupInt( win, "height", 100 );
+        // Read Window User-Config
+        Utils::IniType winConfig;
+        if ( Utils::readIni(
+                 "./config/" + Utils::stringReplace( nextWindow, "/", "_" ), winConfig ) ) {
+
+            win       = Utils::readIniGroup( winConfig, "window" );
+            winX      = Utils::readIniGroupInt( win, "x", 0 );
+            winY      = Utils::readIniGroupInt( win, "y", SDL_WINDOWPOS_CENTERED_DISPLAY( 0 ) );
+            winWidth  = Utils::readIniGroupInt( win, "w", 0 );
+            winHeight = Utils::readIniGroupInt( win, "h", 0 );
+
+            game.scaleWindowToMatchDPI = false;
+        }
+        winConfig.clear();
+
+        if ( winWidth == 0 || winHeight == 0 ) {
+            winWidth = 0; winHeight = 0;
+            game.scaleWindowToMatchDPI = true;
+        }
 
         Scenes::MainScene* ms = nullptr;
-        Engine game;
         try {
-            ms = new Scenes::MainScene( ini, (float)winWidth / 100.0f, (float)winHeight / 100.0f );
+            ms = new Scenes::MainScene( ini, (float)viewWidth / 100.0f, (float)viewHeight / 100.0f );
 
             ret = game.start(
-                argc, argv, winWidth, winHeight, 96.0f,
+                argc, argv, viewWidth, viewHeight, 96.0f,
                 ( "AMP - " + iniPath[ iniPath.size() - 1 ] ).c_str(), ms, 0,
-                SDL_WINDOW_ALWAYS_ON_TOP, 0 );
+                SDL_WINDOW_ALWAYS_ON_TOP, winX, winY, winWidth, winHeight );
+
+            // Save Window Position and Size for next time
+            system( "mkdir -p ./config" );
+            Utils::IniType cfg = { { "window",
+                                     {
+                                         { "x", std::to_string( game.windowPosition.x ) },
+                                         { "y", std::to_string( game.windowPosition.y ) },
+                                         { "w", std::to_string( game.windowSize.x ) },
+                                         { "h", std::to_string( game.windowSize.y ) },
+                                     } } };
+            Utils::writeIni( "./config/" + Utils::stringReplace( nextWindow, "/", "_" ), cfg );
+
+            nextWindow = "";
             nextWindow = ms->getNextProfile();
         }
-        catch( char const* msg ) { game.msgError("Error", msg); }
+        catch ( char const* msg ) {
+            game.msgError( "Error", msg );
+            nextWindow = "";
+        }
 
         delete ini;
-        if(ms) delete ms;
+        if ( ms ) delete ms;
     }
 
     return ret;
